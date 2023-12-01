edaf95f584ba87e06a46a3659b22a315cbe517e6
leedavi <dcl@agence-sesame.fr>
2023-12-01 13:14:49 +0100

    migrate

---
+++ rocketcontentapi.FAQ/1.0/js/faq.js
@@ -0,0 +1,15 @@
+﻿function slideAccordionFAQ(contentId, moduleId) {
+    if ($('.a-active' + moduleId).attr('id') == contentId) {
+        $('.a-active' + moduleId).slideUp();
+        $('.a-active' + moduleId).removeClass('accordion-active');
+        $('.a-active' + moduleId).removeClass('a-active' + moduleId);
+    }
+    else {
+        $('.a-active' + moduleId).slideUp();
+        $('.a-active' + moduleId).removeClass('accordion-active');
+        $('.a-active' + moduleId).removeClass('a-active' + moduleId);
+        $('#' + contentId).slideDown();
+        $('#' + contentId).addClass('accordion-active');
+        $('#' + contentId).addClass('a-active' + moduleId);
+    }
+}
