509fa2c63b3bc49e073b036b91d67928fe8a0cdd
David Lee <dcl@agence-sesame.fr>
2024-11-24 15:51:12 +0100

    RocketContent AppThemes

--- rocketcontentapi.FAQ/1.0/js/faq.js
+++ rocketcontentapi.FAQ/1.0/js/faq.js
@@ -3,6 +3,8 @@
         $('.a-active' + moduleId).slideUp();
         $('.a-active' + moduleId).removeClass('accordion-active');
         $('.a-active' + moduleId).removeClass('a-active' + moduleId);
+        $('.arrowleft' + contentId).hide();
+        $('.arrowright' + contentId).show();
     }
     else {
         $('.a-active' + moduleId).slideUp();
@@ -11,5 +13,7 @@
         $('#' + contentId).slideDown();
         $('#' + contentId).addClass('accordion-active');
         $('#' + contentId).addClass('a-active' + moduleId);
+        $('.arrowleft' + contentId).show();
+        $('.arrowright' + contentId).hide();
     }
 }
