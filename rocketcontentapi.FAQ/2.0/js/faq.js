017b2ef1bc87802578a45b59b2d1349fff240d77
David Lee <dcl@agence-sesame.fr>
2026-02-18 10:38:11 +0100

    v2.0 - For @Editor token

---
+++ rocketcontentapi.FAQ/2.0/js/faq.js
@@ -0,0 +1,19 @@
+﻿function slideAccordionFAQ(contentId, moduleId) {
+    if ($('.a-active' + moduleId).attr('id') == contentId) {
+        $('.a-active' + moduleId).slideUp();
+        $('.a-active' + moduleId).removeClass('accordion-active');
+        $('.a-active' + moduleId).removeClass('a-active' + moduleId);
+        $('.arrowleft' + contentId).hide();
+        $('.arrowright' + contentId).show();
+    }
+    else {
+        $('.a-active' + moduleId).slideUp();
+        $('.a-active' + moduleId).removeClass('accordion-active');
+        $('.a-active' + moduleId).removeClass('a-active' + moduleId);
+        $('#' + contentId).slideDown();
+        $('#' + contentId).addClass('accordion-active');
+        $('#' + contentId).addClass('a-active' + moduleId);
+        $('.arrowleft' + contentId).show();
+        $('.arrowright' + contentId).hide();
+    }
+}
